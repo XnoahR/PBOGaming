08e91e5d5e1b6b5b1429e0a433d6c6213864a82b
salomopm <salomopm26@gmail.com>
2022-10-20 17:07:09 +0700

    nambah

---
+++ Game/musuh.java
@@ -0,0 +1,42 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
+/**
+ * Write a description of class musuh here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class musuh extends Actor
+{
+    
+    public int tahan = 30;
+    private int jeda = 0;
+    
+    public static int randomNumberGenerator(int min, int max){
+    Random r = new Random();
+    double randomNum = r.nextDouble();
+    int result = (int)(randomNum * (max - min)) + min;
+    return result;
+    }
+
+    int speed = randomNumberGenerator(1, 5);
+    public void gerak(int speed){
+        setLocation(getX()+speed, getY());
+        if(getY()>700){
+            setLocation(Greenfoot.getRandomNumber(500), Greenfoot.getRandomNumber(50));
+            
+        }
+        if(getX()>500){
+                getWorld().removeObject(this);
+            }
+    }
+
+    public void act()
+    {
+        gerak(speed);
+        //kena();
+        if(tahan==0){
+            tahan = 30;
+        }
+    }
+}
