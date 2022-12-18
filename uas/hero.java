fbd36b1dc8b709c8d898d01a33b429a0b06b8243
salomopm <salomopm26@gmail.com>
2022-12-18 14:16:49 +0700

    uas1

--- uas/hero.java
+++ uas/hero.java
@@ -12,6 +12,10 @@ public class hero extends Actor
      * Act - do whatever the hero wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    private int Yspeed = 3;
+    private int Xspeed = 3;
+    
     public void act()
     {
         
@@ -25,5 +29,27 @@ public class hero extends Actor
         if(i != null){
             getWorld().removeObject(i);
         }
+    }
+    
+    public void checkKey(Actor i){
+        if(Greenfoot.isKeyDown("left")){
+            if(getX()>=0){
+                setLocation(getX() -2, getY());}
+        }
+        if(Greenfoot.isKeyDown("right")){
+            if(getX()<=1270){
+                setLocation(getX() +2, getY());}
+        }
+        if(Greenfoot.isKeyDown("up")){        
+            if(getY()>=300){
+                setLocation(getX(), getY()-2);
+            }
+        }
+        if(Greenfoot.isKeyDown("down")){        
+            if(getY()<=600){
+                setLocation(getX(), getY()+2);
+            }
+        }
+    
     }
 }
