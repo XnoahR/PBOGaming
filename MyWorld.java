b189b2b829324557b43e15ce04a5dd1ce59a0bbd
XnoahR <rio.ae23@gmail.com>
2022-12-19 13:07:45 +0700

    golem

--- MyWorld.java
+++ MyWorld.java
@@ -8,8 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-    public boolean isMoving = true;
-    static int score = 0;
+
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -17,33 +16,34 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(700, 400, 1); 
-        Character c = new Character();
-        addObject(c,50,300);
-        tampilscore();
-       
+        super(720, 480, 1); 
+        GreenfootImage bg = new GreenfootImage("C:/Users/LENOVO/Downloads/SCENE 1_Goji Town.png");
+        bg.scale(getWidth(), getHeight());
+        setBackground(bg);
+        sorcerer s = new sorcerer(5,5,70,35);
+        s.SizeSet();
+        addObject( s, 0, 300);
+        Fire fireball = new Fire();
+        addObject(fireball,150,300);
+        reaper1 reaper = new reaper1(10,75,-1);
+        reaper.SizeSet();
+        addObject(reaper,600,300);
+        golem1 golem = new golem1(10,75,-1);
+        golem.SizeSet();
+        addObject(golem,600,150);
+        //char1 c = new char1();
+        //addObject(c,50,300);
     }
-    public static int getScore(){
-        return score;
-    }
-    public void addScore(int points){
-        score = score + points;
-        tampilscore();
-    }
-    public void tampilscore(){
-        showText("Score :"+score, 100,25);
-    }
-
     
     public void act(){
-        
-        MyWorld bg = new MyWorld();
-        
-        if(Greenfoot.getRandomNumber(150) < 1){
-            addObject( new musuh(), 700 , Greenfoot.getRandomNumber(200) +200);
-            addObject( new musuh2(), 700 , Greenfoot.getRandomNumber(200) +200);
+        if(Greenfoot.getRandomNumber(750) < 5){
+            addObject( new reaper1(), 700 , Greenfoot.getRandomNumber(200) +200);
+            
         }
-        
+        else if(Greenfoot.getRandomNumber(1000) < 5){
+            addObject( new golem1(), 700 , Greenfoot.getRandomNumber(200) +200);
+        }
     }
-}
 
+
+}
